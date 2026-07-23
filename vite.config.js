2f718fab83a0f672a8f503d07d55cf22c619e09b
Bijay <bijaya.panda@quotus.co.in>
2026-07-23 10:57:03 +0000

    Update project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/angstrohmconsulting.com",
+  base: ["angstrohmconsulting.com"],
   plugins: [
     react(),
     tailwindcss(),
